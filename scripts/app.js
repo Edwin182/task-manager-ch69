2ecc633b7654e473717dbfdb923d6e64cf27be82
Edwin182 <edwinphone182@gmail.com>
2026-07-22 22:07:25 -0700

    assignment 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,77 @@
+
 function saveTask()
-{
-    // get the values from the DOM
+{   
+    //get values from the DOM
     const title = $("#txtTitle").val();
-    const desc = $("txtDescription").val();
-    const color = $("selColor").val();
-    const date = $("selDate").val();
-    const status = $("selStatus").val();
-    const budget = $("numBudget").val();
-    
+    const desc =$("#txtDescription").val();
+    const color =$("#selColor").val();
+    const date =$("#selDate").val();
+    const status =$("#selStatus").val();
+    const budget =$("#numBudget").val();
     //Create the object
     const taskToSave = new Task(title, desc, color, date, status, budget);
     console.log(taskToSave);
+
+    //mock the response from the server
+    displayTask(taskToSave);
+
+    //Send to Server
+    $.ajax({
+        type:"POST",// HTTP Verb : Create
+        url: API,
+        data: JSON.stringify(taskToSave),
+        contentType:"application/json",
+        success:function(created){
+            console.log(created);
+        },
+        error: function(err){
+            console.log(err);
+        }
+    })
 }
 
+const API = "https://106api-b0bnggbsgnezbzcz.westus3-01.azurewebsites.net/api/tasks";
+
+function loadTask(){
+    $.ajax({
+        type:"get", //HTTP Verb. READ
+        url: API,
+        dataType:"json",//Expected format
+        success: function(data){
+            console.log(data);
+        },
+        error:function(err){
+            console.log(err);
+        }
+    })
+}
+
+function displayTask(task){
+let syntax =  `
+    <div class="task" style="border-color:${task.color}">
+        <div class="info">
+            <h4>${task.title}</h4>
+            <p>${task.desc}</p>
+        </div>
+        <label class="status">${task.status}</label>
+    <div class="date-budget">
+        <label>Due: ${task.date}</label>
+        <label>Budget: $${task.budget}</label>
+    </div>
+    </div>`;
+    
+  // Inject the new HTML into the DOM Tree
+    $(".list").append(syntax);
+
+}
 
 function init(){
-    console.log("hello from 106");
+    console.log("Hello from 106");
     $("#btnSave").click(saveTask);
+
+    //load data from the server
+    loadTask();
 }
 
 window.onload = init;
-// It forces the HTML and the CSS gets resolved before the logic gets executed
+//It force that the HTML and the CSS gets resolved before that the logic gets executed
